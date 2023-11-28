0eb5b574642ed51aa00d44f7cbcacb576e46e5e4
vygaslav03 <118902682+vygaslav03@users.noreply.github.com>
2023-11-28 16:02:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@
 */ # # # # # # # # # # # #
 
 provider "aws" {
-  access_key = "AKIA35NNHMKA3QL7EWNK"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
 
